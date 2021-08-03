6ac7c7bd583f5fdc90812c680dc12f453e684e30
Aaron Choo <choozy.aaron@gmail.com>
2021-08-03 20:11:45 +0800

    fix: add cors

--- functions/src/https/register.ts
+++ functions/src/https/register.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as cors from "cors";
 
 import { User } from "../schema/user";
 import { fs, auth } from "../utils/admin";
@@ -15,52 +16,55 @@ type ErrorResponse = {
   details?: unknown;
 };
 
-export const register = functions
-  .region("asia-southeast2")
-  .https.onRequest(async (request, response): Promise<void> => {
-    const { username, email, password }: RegisterRequest = request.body;
+const corsHandler = cors({ origin: true });
 
-    // check required data is present
-    if (!username || !email || !password) {
-      response.status(400).json({
-        code: "invalid-argument",
-        message: "All fields must be prsent: username, email, and password.",
-      } as ErrorResponse);
-      return;
-    }
+export const register = functions.region("asia-southeast2").https.onRequest(
+  async (request, response): Promise<void> =>
+    corsHandler(request, response, async () => {
+      const { username, email, password }: RegisterRequest = request.body;
 
-    // check if username already exists
-    const userSnapshot = await fs
-      .collection("user")
-      .where("username", "==", username)
-      .get();
-    if (!userSnapshot.empty) {
-      response.status(400).json({
-        code: "already-exists",
-        message: "The username is already in use by another account.",
-      } as ErrorResponse);
-      return;
-    }
+      // check required data is present
+      if (!username || !email || !password) {
+        response.status(400).json({
+          code: "invalid-argument",
+          message: "All fields must be prsent: username, email, and password.",
+        } as ErrorResponse);
+        return;
+      }
 
-    try {
-      // create the auth user
-      const userCreated = await auth.createUser({
-        email: email,
-        password: password,
-      });
+      // check if username already exists
+      const userSnapshot = await fs
+        .collection("user")
+        .where("username", "==", username)
+        .get();
+      if (!userSnapshot.empty) {
+        response.status(400).json({
+          code: "already-exists",
+          message: "The username is already in use by another account.",
+        } as ErrorResponse);
+        return;
+      }
 
-      // create the fs user doc
-      const userDoc: User = {
-        username: username,
-        email: email,
-        vouchers: [],
-        shops: [],
-      };
-      await fs.collection("user").doc(userCreated.uid).set(userDoc);
+      try {
+        // create the auth user
+        const userCreated = await auth.createUser({
+          email: email,
+          password: password,
+        });
 
-      response.status(204).send();
-    } catch (err) {
-      // errors if any
-      response.status(400).json(err as ErrorResponse);
-    }
-  });
+        // create the fs user doc
+        const userDoc: User = {
+          username: username,
+          email: email,
+          vouchers: [],
+          shops: [],
+        };
+        await fs.collection("user").doc(userCreated.uid).set(userDoc);
+
+        response.status(204).send();
+      } catch (err) {
+        // errors if any
+        response.status(400).json(err as ErrorResponse);
+      }
+    })
+);
